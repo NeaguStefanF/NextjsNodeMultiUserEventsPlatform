c594d4c7fd997b605601c36fb2bbbe199f00b86e
NeaguStefanF <neagu.stefan.fanel@gmail.com>
2023-11-23 14:41:34 +0200

    fixing export app

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const formRoutes = require('./routes/form');
 const comunityRoutes = require('./routes/comunity');
 
 //app
-const app = express();
+export const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
 
@@ -49,5 +49,3 @@ const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-export default app;
