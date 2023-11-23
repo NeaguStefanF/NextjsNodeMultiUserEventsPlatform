439ac78b2a8cd223834955141be1cf6d2139ddc2
NeaguStefanF <neagu.stefan.fanel@gmail.com>
2023-11-23 15:08:25 +0200

    vercel

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const formRoutes = require('./routes/form');
 const comunityRoutes = require('./routes/comunity');
 
 //app
-export const app = express();
+const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
 
@@ -49,3 +49,5 @@ const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+export default app;
